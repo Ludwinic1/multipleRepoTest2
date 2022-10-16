0b7f2deca84a2fae9b5e6ffb1235d566f7e6fb27
Nick Ludwig <nick.ludwig223@gmail.com>
2022-10-16 10:17:00 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='My Setup File',
+      version='1.0',
+      description='Python Multiple Repos Test',
+      author='Nick Ludwig',
+      author_email='nick...',
+      packages=find_packages()
+     )
